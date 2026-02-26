5961be863b2be1a64de54bf4ff46ae6e822d6203
Zhanik <zhako120313@gmail.com>
2026-02-27 01:19:33 +0600

    chore(prisma): add user first/last/nickname fields

---
+++ prisma/migrations/20260226191857_add_user_profile_fields/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[nickname]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "nickname" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
