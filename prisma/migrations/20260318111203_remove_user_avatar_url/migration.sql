82a6732c6d00cf2104f010333196c551fdde6a52
Zhanik <zhako120313@gmail.com>
2026-03-18 17:13:51 +0600

    refactor(db): drop legacy user avatarUrl column

---
+++ prisma/migrations/20260318111203_remove_user_avatar_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatarUrl` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "avatarUrl";
